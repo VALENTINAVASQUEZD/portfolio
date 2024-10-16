8a8c8a2bfc6a18ae51db3de05d43e9d2f6be3760
valentina <mavalede@gmail.com>
2024-10-16 08:13:33 -0500

    cambio de color

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 
 export default function Home() {
   return (
-    <main className="flex justify-center items w-screen h-screen bg-cyan-300">
-      <h1  className="text-4xl text-cyan-800 front-extrabold"> My Portfolio  </h1>
+    <main className="flex justify-center items w-screen h-screen bg-[#fef2f3]">
+      <h1 className="text-4xl text-cyan-800 font-extrabold"> My Portfolio </h1>
     </main>
   );
 }
+
